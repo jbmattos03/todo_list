2799f300776c6c25dbad30b6ee977017f35af89e
jbmattos03 <jbmattos2003@gmail.com>
2025-06-02 18:18:03 -0300

    feat: Add user and task models

---
+++ src/Models/taskModel.js
@@ -0,0 +1,52 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../Database/database.js";
+import User from "./userModel.js";
+
+class Task extends Model {
+    static associate(models) {
+        this.belongsTo(models.User, {
+            foreignKey: {
+                name: "userId",
+                allowNull: false, // Garantir participação total
+            },
+            onDelete: "CASCADE", // Excluir tarefas associadas ao usuário
+            onUpdate: "CASCADE", // Atualizar tarefas se o usuário for atualizado
+            as: "user", // Nome da associação
+        });
+    }
+}
+
+Task.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    status: {
+        type: DataTypes.ENUM("pending", "completed"),
+        defaultValue: "pending",
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User, // Referência ao modelo User
+            key: "id",
+        },
+    },
+}, {
+    sequelize,
+    modelName: "Task",
+    tableName: "tasks",
+    timestamps: true, // Adiciona createdAt e updatedAt
+});
+
+export default Task;
---
+++ src/Models/userModel.js
@@ -0,0 +1,44 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../Database/database.js";
+
+class User extends Model {
+    static associate(models) {
+        this.hasMany(models.Task, {
+            foreignKey: {
+                name: "userId",
+                allowNull: false, // Garantir participação total
+            },
+            onDelete: "CASCADE", // Excluir tarefas associadas ao usuário
+            onUpdate: "CASCADE", // Atualizar tarefas se o usuário for atualizado
+            as: "tasks", // Nome da associação
+        });
+    }
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+    },
+}, {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+    timestamps: true, // Adiciona createdAt e updatedAt
+})
+
+export default User;
